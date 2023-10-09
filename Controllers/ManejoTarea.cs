b13f6a218ab497adeb0d80cfd2c69c7b49c195c4
josepro752 <joseignacioboggio@gmail.com>
2023-10-09 16:00:23 -0300

    subo ultimos cambios

--- Controllers/ManejoTarea.cs
+++ Controllers/ManejoTarea.cs
@@ -16,68 +16,80 @@ public class ManejoTareaController : ControllerBase
         manejoTarea = new ManejoTarea(accesoADatos);
     }
 
-// ● Crear una nueva tarea.
-// ● Obtener una tarea por id.
-// ● Actualizar una tarea.
-// ● Eliminar una tarea.
-// ● Listar todas las tareas.
-// ● Listar todas las tareas completadas.
+    // ● Crear una nueva tarea.
+    // ● Obtener una tarea por id.
+    // ● Actualizar una tarea.
+    // ● Eliminar una tarea.
+    // ● Listar todas las tareas.
+    // ● Listar todas las tareas completadas.
 
     [HttpPost]
-    [Route ("CrearTarea")]
-    public ActionResult<Tarea> CrearTarea(Tarea tarea) {
+    [Route("CrearTarea")]
+    public ActionResult<Tarea> CrearTarea(Tarea tarea)
+    {
         var t = manejoTarea.CrearTarea(tarea);
-        if (t != null) {
+        if (t != null)
+        {
             return Ok(t);
         }
         return BadRequest(t);
     }
 
     [HttpGet]
-    [Route ("GetTareaID")]
-    public ActionResult<Tarea> GetTareaID(int id) {
+    [Route("GetTareaID")]
+    public ActionResult<Tarea> GetTareaID(int id)
+    {
         var t = manejoTarea.GetTareaID(id);
-        if (t != null) {
+        if (t != null)
+        {
             return Ok(t);
         }
         return BadRequest(t);
     }
 
     [HttpPut]
-    [Route ("ActualizarTarea")]
-    public ActionResult<Tarea> ActualizarTarea(int id, EstadoTarea estadoTarea) {
-        var t = manejoTarea.ActualizarTarea(id,estadoTarea);
-        if (t != null) {
+    [Route("ActualizarTarea")]
+    public ActionResult<Tarea> ActualizarTarea(int id, EstadoTarea estadoTarea)
+    {
+        var t = manejoTarea.ActualizarTarea(id, estadoTarea);
+        if (t != null)
+        {
             return Ok(t);
         }
         return BadRequest(t);
     }
 
     [HttpDelete]
-    [Route ("EliminarTarea")]
-    public ActionResult<Tarea> EliminarTarea(int id) {
+    [Route("EliminarTarea")]
+    public ActionResult<Tarea> EliminarTarea(int id)
+    {
         var t = manejoTarea.EliminarTarea(id);
-        if (t) {
+        if (t)
+        {
             return Ok();
         }
         return BadRequest();
     }
 
     [HttpGet]
-    [Route ("GetTareas")]
-    public ActionResult<Tarea> GetTareas() {
+    [Route("GetTareas")]
+    public ActionResult<Tarea> GetTareas()
+    {
         var t = manejoTarea.GetTareas();
-        if (t != null) {
+        if (t != null)
+        {
             return Ok(t);
         }
         return BadRequest(t);
     }
 
     [HttpGet]
-    [Route ("GetTareasCompletadas")]
-    public ActionResult<Tarea> GetTareasCompletadas() {
+    [Route("GetTareasCompletadas")]
+    public ActionResult<Tarea> GetTareasCompletadas()
+    {
         var t = manejoTarea.GetTareasCompletadas();
-        if (t != null) {
+        if (t != null)
+        {
             return Ok(t);
         }
         return BadRequest(t);
